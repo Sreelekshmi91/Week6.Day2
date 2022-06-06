08c99100c299cfbaf678c7ba5cad3afe6b721a92
Sreelekshmi91 <102054755+Sreelekshmi91@users.noreply.github.com>
2022-06-06 23:52:58 +0530

    Add files via upload

---
+++ runner1/CucumberRunner.java
@@ -0,0 +1,14 @@
+package runner1;
+
+import io.cucumber.testng.CucumberOptions;
+import steps.BaseClass;
+
+@CucumberOptions(features = { "src/test/java/features_1" }, glue = "steps", monochrome = true, publish = true,
+		// tags = "@functional"
+		// tags = "@sanity or @regression"
+		// tags = "@functional or @smoke"
+		// tags = "@smoke and @regression"
+		// tags = "@regression"
+		tags = "@smoke and @sanity")
+public class CucumberRunner extends BaseClass {
+}
